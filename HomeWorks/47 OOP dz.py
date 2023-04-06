6b45a83cb445c1cbf052f27852cce0694598e590
Tanly1995 <orik_05_2000@mail.ru>
2023-04-07 00:41:38 +0400

    HW all done

--- HomeWorks/47 OOP dz.py
+++ HomeWorks/47 OOP dz.py
@@ -1,3 +1,4 @@
+#1 zadanie
 '''class BankAccount:
     def __init__(self, balance):
         self.__balance = balance
@@ -35,11 +36,38 @@ print(employee.get_salary())'''
     def get_grades(self):
         return self.__grades
 
-    def add_grades(self):
-        self.add_grades()
+    def add_grades(self, new_grade):
+        self.__grades = new_grade
 
 
 student = Student(10)
+student.add_grades(7) #Введи новое число
 print(student.get_grades())'''
 
 #4 zadanie
+'''class Car:
+    def __init__(self, brand, model, year):
+        self.__brand = brand
+        self.__model = model
+        self.__year = year
+
+    def get_car_info(self):
+        print(self.__brand, self.__model, self.__year)
+
+car = Car("BMW", "M3 GTR", 2002)
+car.get_car_info()'''
+
+#5 zadanie
+'''class Game:
+    def __init__(self, score):
+        self.__score = score
+
+    def update_score(self, value):
+        self.__score = value
+
+    def get_score(self):
+        return self.__score
+
+game = Game(2100)
+game.update_score(3400)
+print(game.get_score())'''
