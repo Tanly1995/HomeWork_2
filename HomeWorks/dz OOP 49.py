2ca457a36ce0784171a2b7f09997fcc00afa1b25
Tanly1995 <orik_05_2000@mail.ru>
2023-04-10 01:35:07 +0400

    Voprosi est

---
+++ HomeWorks/dz OOP 49.py
@@ -0,0 +1,19 @@
+#1 zadanie
+'''class Cars:
+    def __init__(self, model, year, city, engine, color, cost):
+        self.model = model
+        self.year = year
+        self.city = city
+        self.engine = engine
+        self.color = color
+        self.cost = cost
+    def info(self):
+        print(self.model, self.year, self.city, self.engine, self.color, self.cost)
+
+cars = Cars(input("model: "), input("year: "), input("city :"), float(input("engine: ")), input("color: "), input("cost: "))
+cars.info()'''
+
+#2 zadanie
+#3 zadanie
+#4 zadanie
+#5 zadanie
