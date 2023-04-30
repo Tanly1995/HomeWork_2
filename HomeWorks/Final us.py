02a395fb9ff1c5e78c158b9e3c20ac5c542a6d0e
Tanly1995 <orik_05_2000@mail.ru>
2023-05-01 01:18:21 +0400

    finalroject

--- HomeWorks/Final us.py
+++ HomeWorks/Final us.py
@@ -8,8 +8,8 @@ class PinPage(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.title("Poveseniy")
-        self.master.config(bg='#faf2d5')
+        self.master.title("Poveseniy - The Game")
+        self.master.config(bg='#977016')
         self.master.geometry('220x100')
         self.widgets()
 
@@ -26,24 +26,26 @@ class PinPage(Frame):
         self.master.withdraw()
 
     def widgets(self):
-        welcome = Label(self.master, text='Enter pin code', font=("Open Sans", 12)).grid(row=1, column=2)
+        welcome = Label(self.master, text='Enter pin code',bg='#977016' ,font=("Open Sans", 12, "bold")).grid(row=1, column=2)
 
         self.pin_code = Entry(self.master, justify=CENTER, bd=4)
         self.pin_code.grid(row=2, column=2)
 
         self.btn_next = Button(self.master, text='Next', command=self.Next, font=("Open Sans", 12))
-        self.btn_next.grid(row=3, column=1, sticky=W)
+        self.btn_next.grid(row=3, column=3, sticky=E)
         self.btn_exit = Button(self.master, text="Exit", command=self.Exit, font=("Open Sans", 12))
-        self.btn_exit.grid(row=3,column=3, sticky=E)
+        self.btn_exit.grid(row=3,column=1, sticky=W)
+
+        root.iconbitmap(default=r"D:\Python works\Lessons\hanged_ico.ico")
 
 class Viselnik(Frame):
 
     # Список слов для угадывания
-    words = ['python', 'programming', 'computer', 'code', 'algorithm', 'debugging']
+    list_words = ['python', 'car', 'computer', 'teacher', 'step', 'helicopter']
     # Количество попыток
     tries = 5
     # Выбор случайного слова
-    word = choice(words)
+    word = choice(list_words)
     # Список угаданных букв
     guessed_letters = []
     # Количество картинок
@@ -57,10 +59,10 @@ class Viselnik(Frame):
     def __init__(self,master = None):
         super().__init__(master)
         self.master1 = master
-        self.master1.title('Виселица')
+        self.master1.title('Poveseniy - The Game')
         self.master1.geometry('800x400')
         self.master1.resizable(False, False)
-        self.master1['bg'] = '#DCDCDC'
+        self.master1['bg'] = '#977016'
         self.create_widgets()
 
 
@@ -71,13 +73,13 @@ class Viselnik(Frame):
         self.label_hangman = Label(self.master1, image=self.hangman_image)
         self.label_hangman.pack()
 
-        self.label_word = Label(self.master1, text=self.get_word_display(), font=('Arial', 20))
+        self.label_word = Label(self.master1, text=self.get_word_display(),bg='#977016', font=('Open Sans', 20))
         self.label_word.pack()
 
-        self.label_tries = Label(self.master1, text=f'Количество оставшихся попыток: {self.tries}')
+        self.label_tries = Label(self.master1, text=f'Осталось {self.tries} попыток', bg='#977016')
         self.label_tries.pack()
 
-        self.label_result = Label(self.master1, text='')
+        self.label_result = Label(self.master1, text='', bg='#977016')
         self.label_result.pack()
 
         for letter in 'abcdefghijklmnopqrstuvwxyz':
@@ -98,15 +100,17 @@ class Viselnik(Frame):
             self.guessed_letters.append(letter)
             self.label_word.config(text=self.get_word_display())
             if set(self.guessed_letters) == set(self.word):
-                self.label_result.config(text='Вы победили!')
+                self.label_result.config(text='Вы победили!', bg='#977016', font=("Open Sans", 12, 'bold'))
         else:
             self.tries -= 1
-            self.label_tries.config(text=f'Количество оставшихся попыток: {self.tries}')
+            self.label_tries.config(text=f'Осталось {self.tries} попыток',font=("Open Sans", 12, 'bold'), bg='#977016' )
             if self.tries == 0:
-                self.label_result.config(text=f'Вы проиграли! Загаданное слово: {self.word}')
+                self.label_result.config(text=f'Вы проиграли! Слово: {self.word}', font=("Open Sans", 12, 'bold'),bg='#977016')
                 # Update the hangman image
                 self.hangman_image = ImageTk.PhotoImage(Image.open(self.images[-1]).resize((200, 200)))
                 self.label_hangman.config(image=self.hangman_image)
+            elif self.tries < 0:
+                self.label_tries.config(text=f'Прекрати тыкать',font=("Open Sans", 12, 'bold'))
             else:
                 misses = 5 - self.tries
                 self.hangman_image = ImageTk.PhotoImage(Image.open(self.images[misses]).resize((200, 200)))
